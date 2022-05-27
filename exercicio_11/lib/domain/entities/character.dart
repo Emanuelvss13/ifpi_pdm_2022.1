91c91ec0847bc546ca186e523dfa95b4a6581cd7
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-05-27 10:27:40 -0300

    exercicio_11: entidade

---
+++ exercicio_11/lib/domain/entities/character.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class Character extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String origin;
+  final String url;
+
+  const Character(
+      {required this.id,
+      required this.name,
+      required this.status,
+      required this.species,
+      required this.type,
+      required this.gender,
+      required this.origin,
+      required this.url});
+
+  @override
+  List<Object?> get props =>
+      [id, name, status, species, type, gender, origin, url];
+}

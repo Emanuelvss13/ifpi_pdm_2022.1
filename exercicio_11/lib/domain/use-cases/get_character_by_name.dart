ce09983ac184971b12618ceb347418fd9b9dee14
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-05-27 10:34:27 -0300

    exercicio_11: use cases

---
+++ exercicio_11/lib/domain/use-cases/get_character_by_name.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:exercicio_11/core/errors/failure.dart';
+import 'package:exercicio_11/domain/entities/character.dart';
+import 'package:exercicio_11/domain/repositories/ricky_and_morty_repository.dart';
+
+class GetCharacterByName {
+  final RickyAndMortyRepository repository;
+
+  GetCharacterByName(this.repository);
+
+  Future<Either<Failure, Character>> execute({required String name}) async {
+    return await repository.getCharacterByName(name);
+  }
+}
---
+++ exercicio_11/lib/domain/use-cases/get_random_character.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:exercicio_11/core/errors/failure.dart';
+import 'package:exercicio_11/domain/entities/character.dart';
+import 'package:exercicio_11/domain/repositories/ricky_and_morty_repository.dart';
+
+class GetRandomCharacter {
+  final RickyAndMortyRepository repository;
+
+  GetRandomCharacter(this.repository);
+
+  Future<Either<Failure, Character>> execute() async {
+    return await repository.getRandomCharacter();
+  }
+}

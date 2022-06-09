199f1fc5c6b71ca07fe253f6e6072e2c262e10ad
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-06-09 15:15:13 -0300

    feat: implementação do network info

--- exercicio_11/lib/core/platform/network_info.dart
+++ exercicio_11/lib/core/platform/network_info.dart
@@ -1,3 +1,16 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:data_connection_checker/data_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl extends NetworkInfo {
+  DataConnectionChecker connectionChecker;
+
+  NetworkInfoImpl({required this.connectionChecker});
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
 }
